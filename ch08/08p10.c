564df5602a1b5a7947ccf90c337c2d60879c934e
ten2zen <marcospoon@gmail.com>
2017-03-03 15:37:03 -0300

    Update 08p10.c

--- ch08/08p10.c
+++ ch08/08p10.c
@@ -1,65 +1,65 @@
-/*
- * Ch08 Pr10 | C Programming, A Modern Approach
- *
- * Modify Programming Project 8 from Chapter 5 so that the departure times
- * are stored in an array and the arrival times are stored in a second array.
- * (The times are integers, representing the number of minutes since midnight.)
- * The program will use a loop to search the array of departure times for the
- * one closest to the time entered by the user.
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/* The program uses a loop to search the array of departure times
+for the one closest to the time entered by user. Departure and
+arrival times should be stored in arrays. */
 
-#define N_DEPARTURES 8
-#define N_ARRIVALS 8
+#include <stdio.h>
 
 int main(void)
 {
-	int h, m, t, closest_departure, closest_arrival, min_delta;
-	int dep_h, dep_m, arr_h, arr_m;
-	char dep_suffix[] = "a.m.", arr_suffix[] = "a.m.";
-	int departures[8] = { 8 * 60 + 0, 9 * 60 + 43, 11 * 60 + 19,
-			      12 * 60 + 47, 14 * 60 + 0, 15 * 60 + 45,
-			      19 * 60 + 0, 21 * 60 + 45 };
-	int arrivals[8] = { 10 * 60 + 16, 11 * 60 + 52, 13 * 60 + 31,
-			    15 * 60 + 0, 16 * 60 + 8, 17 * 60 + 55,
-			    21 * 60 + 20, 23 * 60 + 58 };
+	/* Store departure times in an array */
+    int departures[8] = {8 * 60, 9 * 60 + 43, 11 * 60 + 19,
+                       12 * 60 + 47, 14 * 60, 14 * 60 + 45,
+                       19 * 60, 21 * 60 + 45};
 
+    /* Store arrival times in an array */
+    int arrivals[8] = {10 * 60 + 16, 11 * 60 + 52, 13 * 60 + 31,
+                     15 * 60, 16 * 60 + 8, 17 * 60 + 55,
+                     21 * 60 + 20, 23 * 60 + 58};
+
+    int i, x, y, delta;
+	int hour, minute;
+	int enteredTimeSinceMidnight;
+	int closestDeparture = departures[0];
+	int closestArrival = arrivals[0];
+
+	/* Prompt user for desired departure time */
 	printf("Enter a 24-hour time: ");
-	scanf("%d:%d", &h, &m);
-	t = h * 60 + m;
-	closest_departure = departures[0];
-	closest_arrival = arrivals[0];
-	min_delta = abs(t - departures[0]);
+	scanf("%d:%d", &hour, &minute);
 
-	for (int i = 0; i < N_DEPARTURES; i++) {
-		if (abs(t - departures[i]) < min_delta) {
-			closest_departure = departures[i];
-			closest_arrival = arrivals[i];
-			min_delta = abs(t - departures[i]);
-		}
-	}
+	/* Calculate entered time to time since midnight */
+	enteredTimeSinceMidnight = (hour * 60) + minute;
 
-	dep_h = closest_departure / 60 % 12;
-	dep_h = dep_h == 0 ? 12 : dep_h;
-	dep_m = closest_departure % 60;
+	/* Calculate delta */
+	delta = enteredTimeSinceMidnight - departures[0];
 
-	if (closest_departure / 60 >= 12) {
-		strcpy(dep_suffix, "p.m.");
-	}
+	printf("Closest departure time is ");
 
-	arr_h = closest_arrival / 60 % 12;
-	arr_h = arr_h == 0 ? 12 : arr_h;
-	arr_m = closest_arrival % 60;
-	
-	if (closest_arrival / 60 >= 12) {
-		strcpy(arr_suffix, "p.m.");
-	}
+	/* Search for the closest departure time */
+    for (i = 0; i < 8; i++)
+    {
+        if ((enteredTimeSinceMidnight - departures[i]) < delta)
+        {
+            closestDeparture = departures[i];
+            closestArrival = arrivals[i];
+            delta = enteredTimeSinceMidnight - departures[i];
+        }
+    }
 
-	printf("Closest departure time is %d:%.2d %s, arriving at %d:%.2d %s\n",
-	       dep_h, dep_m, dep_suffix,
-	       arr_h, arr_m, arr_suffix);
+	printf("%.2d:%.2d", closestDeparture / 60, closestDeparture % 60);
+
+	if (closestDeparture / 60 < 12)
+		printf("am");
+	else
+		printf("pm");
+	printf("\nArriving at %d:%.2d", closestArrival / 60, closestArrival % 60);
+
+	if (closestArrival / 60 < 12)
+		printf("am");
+	else
+		printf("pm");
+
+    putchar('\n');
+
 
 	return 0;
 }
